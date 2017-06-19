1014ea8e1034ca47a41f96ffaf80c50d824a331e
laurent berthelot <laurent.berthelot1969@gmail.com>
2017-06-19 14:56:08 +0200

    test lolo

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 echo "ici l'équipe de winners";
+
+echo '2 eme test';
+
 <<<<<<< HEAD
 echo 'test demodif de laurent';
 =======
@@ -8,4 +11,5 @@ echo 'test demodif de laurent';
 echo "modif Boris";
 
 echo "modif Baptiste";
->>>>>>> 3e31f518b30dbc8fa5b94ab906578b463d4f9477
+>>>>>>> 3e31f518b30dbc8fa5b94ab906578b463d4f9477<<
+echo
