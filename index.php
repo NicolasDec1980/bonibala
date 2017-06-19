1d8e9dd800b7090ce231657746e43872dab92ab1
Boris <bo.babin15@gmail.com>
2017-06-19 16:17:59 +0200

    modif B

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "Je suis dans la branche boris";
 echo "ici l'équipe de winners";
 
 echo '2 eme test';
- echo 'test de laurent avec branche : laurent_branche1';
+echo 'test de laurent avec branche : laurent_branche1';
 
 
 echo "modif Boris";
