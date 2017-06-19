b6f8e9641cb0471b9adce46962e1089692ecc019
laurent berthelot <laurent.berthelot1969@gmail.com>
2017-06-19 15:54:56 +0200

    kjhgkjhk

--- index.php
+++ index.php
@@ -5,12 +5,7 @@ echo "ici l'équipe de winners";
 echo '2 eme test';
  echo 'test de laurent avec branche : laurent_branche1';
 
-<<<<<<< HEAD
-echo 'test demodif de laurent';
-=======
 
 echo "modif Boris";
 
 echo "modif Baptiste";
->>>>>>> 3e31f518b30dbc8fa5b94ab906578b463d4f9477<<
-echo
